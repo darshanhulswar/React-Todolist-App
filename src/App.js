750df114dc15059219272e1a2aef0fc295786ba5
Darshan <dhulswar591@gmail.com>
2020-10-08 14:07:58 +0530

    todo component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
+import Todos from './components/Todos';
+
 import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Hi I'm React App Learning React</h1>
-//     </div>
-//   );
-// }
 
 class App extends Component {
-  render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I'm React App With Extended Class</h1>
-    //   </div>
-    // )
 
-    return React.createElement('div', { className: 'App' },
-      React.createElement('h1', null, "Hello does this work??")
-    )
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with friends',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} />
+      </div>
+    );
   }
 }
 
